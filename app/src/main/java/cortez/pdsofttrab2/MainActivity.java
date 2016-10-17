45132b8f6c807e1e464d7c3c7767e22d60270c70
Victor Cortez <ctovictor@gmail.com>
2016-10-17 03:13:38 -0300

    auto-login implementado

--- app/src/main/java/cortez/pdsofttrab2/MainActivity.java
+++ app/src/main/java/cortez/pdsofttrab2/MainActivity.java
@@ -1,6 +1,7 @@
 package cortez.pdsofttrab2;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -16,10 +17,17 @@ public class MainActivity extends AppCompatActivity {
 
     //field
     Button newUserButton;
+    Button loginButton;
     TextView usernameTextView;
     List<User> users = new LinkedList<User>();
     DBAdapter db = new DBAdapter(this);
     TextView passwordTextView;
+    String encryptedPassword;
+
+    private SharedPreferences autoLoginPref;
+    private SharedPreferences.Editor autoLoginPrefEditor;
+    boolean autologin;
+
 
 
 
@@ -29,8 +37,28 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         newUserButton = (Button) findViewById(R.id.newUserButton);
+        loginButton = (Button) findViewById(R.id.loginButton);
         usernameTextView = (TextView) findViewById(R.id.userEditText);
         passwordTextView = (TextView) findViewById(R.id.senhaEditText);
+
+        autoLoginPref = getSharedPreferences("autoLogin",0);
+        autoLoginPrefEditor = autoLoginPref.edit();
+
+        //função auto-login
+        autologin = autoLoginPref.getBoolean("autoLogin",false);
+
+        //////// set to false due developing process
+        autologin = false;
+        /////// end of corrective code
+
+        if(autologin == true){
+            usernameTextView.setText(autoLoginPref.getString("username",""));
+            loginButton.performClick();
+       }
+
+
+
+
     }
 
     public void onClick(View v){
@@ -43,17 +71,43 @@ public class MainActivity extends AppCompatActivity {
             case R.id.loginButton:
                 //checando se a senha confere com o usuario
                 users = db.searchFor("usuario",usernameTextView.getText().toString(),"tabela");
-                String senhaDigitada = passwordTextView.getText().toString();
-                if(users.get(0).getSenha().equals(Security.encrypt(passwordTextView.getText().toString()))){
-                    Intent loginIntent = new Intent(this, UserScreen.class);
-                    loginIntent.putExtra("username", usernameTextView.getText().toString());
-                    startActivity(loginIntent);
-                }
-                else {
-                    Toast t = Toast.makeText(this, "Usuario ou senha incorretos", Toast.LENGTH_LONG);
+                if(users.size() == 0){
+                    Toast t = Toast.makeText(this, "Usuario nao encrontado", Toast.LENGTH_LONG);
                     t.show();
+                }
+                else {//usuario encontrado
+
+                    //auto filling password if auto-login enabled
+                    if(autologin == true) {
+                        encryptedPassword = autoLoginPref.getString("password","");
+                    }
+                    else{//no auto login
+                        encryptedPassword = Security.encrypt(passwordTextView.getText().toString());
+                    }
+
+                    if (users.get(0).getSenha().equals(encryptedPassword)) {
+
+                        //salvando usuario e senha para proxima inicialização do aplicativo
+                        autoLoginPrefEditor.putBoolean("autoLogin",true);
+                        autoLoginPrefEditor.putString("username",usernameTextView.getText().toString());
+                        autoLoginPrefEditor.putString("password",encryptedPassword);
+                        autoLoginPrefEditor.commit();
+
+                        Intent loginIntent = new Intent(this, UserScreen.class);
+                        loginIntent.putExtra("username", usernameTextView.getText().toString());
+                        startActivity(loginIntent);
+                    } else {
+                        Toast t = Toast.makeText(this, "Usuario ou senha incorretos", Toast.LENGTH_LONG);
+                        t.show();
+                    }
                 }
                 break;
         }
     }
+
+//    private void autoLogin (){
+//        Intent loginIntent = new Intent(this, UserScreen.class);
+//        loginIntent.putExtra("username", autoLoginPref.getString("username",""));
+//        startActivity(loginIntent);
+//    }
 }
