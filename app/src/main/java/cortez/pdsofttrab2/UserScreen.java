d1817a879434899343209bf75f7c9863f1061f3f
izass <izass1312@gmail.com>
2016-10-20 00:49:52 -0300

    Update UserScreen.java
    UserScreem by izass

--- app/src/main/java/cortez/pdsofttrab2/UserScreen.java
+++ app/src/main/java/cortez/pdsofttrab2/UserScreen.java
@@ -15,8 +15,24 @@ public class UserScreen extends AppCompatActivity {
 
     DBAdapter db = new DBAdapter(this);
     User user;
+    TextView userEdittext;
+    TextView senhaEdittext;
+    TextView nomeEdittext;
+    //contato
+    TextView ruaEdittext;
+    TextView bairroEdittext;
+    TextView complementoEdittext;
+    TextView numeroEdittext;
+    TextView cepEdittext;
+    TextView cidadeEdittext;
+    TextView estadoEdittext;
+    //contato
+    TextView celularEdittext;
+    TextView mailEdittext;
+    //imagem
     ImageView imageView;
-    TextView ruaTextView;
+
+
     String username;
     List<User> users = new LinkedList<User>();
 
@@ -26,20 +42,41 @@ public class UserScreen extends AppCompatActivity {
         setContentView(R.layout.activity_user_screen);
 
         imageView = (ImageView) findViewById(R.id.imageViewShow);
-        ruaTextView = (TextView) findViewById(R.id.ruaEditText);
-
+        userEdittext = (TextView) findViewById(R.id.userEditText);
+        nomeEdittext = (TextView) findViewById(R.id.nomeEditText);
+        //endereço
+        ruaEdittext = (TextView) findViewById(R.id.ruaEditText);
+        bairroEdittext = (TextView) findViewById(R.id.bairroEditText);
+        complementoEdittext = (TextView) findViewById(R.id.complementoEditText);
+        numeroEdittext = (TextView) findViewById(R.id.numeroEditText);
+        cepEdittext = (TextView) findViewById(R.id.cepEditText);
+        cidadeEdittext = (TextView) findViewById(R.id.cidadeEditText);
+        estadoEdittext = (TextView) findViewById(R.id.estadoEditText);
+        //contato
+        celularEdittext = (TextView) findViewById(R.id.celularEditText);
+        mailEdittext = (TextView) findViewById(R.id.mailEditText);
         //pega o nome de usuario que logou
         Bundle extra = getIntent().getExtras();
         username = extra.getString("username");
-
-
 
         users = db.searchFor("usuario",username,"tabela");
         //usuario que estamos lidando foi selecionado
         user = users.get(0);
 
         // exemplo para rua pegando o nome de usuario e mostrando(errado, consertar isaac)
-        ruaTextView.setText(user.getUser() + " id: "+ user.getId());
+        userEdittext.setText(user.getUser());
+        nomeEdittext.setText(user.getNome());
+        //adress
+        ruaEdittext.setText(user.getRua());
+        bairroEdittext.setText(user.getBairro());
+        complementoEdittext.setText(user.getComplemento());
+        cepEdittext.setText(user.getCep());
+        numeroEdittext.setText(user.getNumero());
+        cidadeEdittext.setText(user.getCidade());
+        estadoEdittext.setText(user.getEstado());
+        //contato
+        celularEdittext.setText(user.getCelular());
+        mailEdittext.setText(user.getMail());
 
         //transformando bytearray em bitmap para poder exibir
         Bitmap bitmap = BitmapFactory.decodeByteArray(user.getFoto(),0,user.getFoto().length);
